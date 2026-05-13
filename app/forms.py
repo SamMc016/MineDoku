2c1f14453e8e853fe081ef7af39fb048fccb510a
Benjamin Holliday <Ben.H321@outlook.com>
2026-05-13 12:48:28 +0800

    added secure login system with CSRF

---
+++ app/forms.py
@@ -0,0 +1,33 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, EmailField
+from wtforms.validators import DataRequired, Email, EqualTo, Length
+
+
+class LoginForm(FlaskForm):
+    username = StringField("Username or Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Go")
+
+
+class SignupForm(FlaskForm):
+    username = StringField("Username", validators=[
+        DataRequired(),
+        Length(min=3, max=20)
+    ])
+
+    email = EmailField("Email", validators=[
+        DataRequired(),
+        Email()
+    ])
+
+    password = PasswordField("Password", validators=[
+        DataRequired(),
+        Length(min=8)
+    ])
+
+    confirm_password = PasswordField("Confirm Password", validators=[
+        DataRequired(),
+        EqualTo("password")
+    ])
+
+    submit = SubmitField("Create Account")
--- minedoku.py
+++ minedoku.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
 
         populate_blocks()
         populate_conditions()
-        
-    app.run(debug=True)
+    
+    app.run(debug=True, port=8000)
