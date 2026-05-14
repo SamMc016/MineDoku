f45fe607400e41267065f02d188f25d56c71080b
Hamsa Yusuf <hamsayusuf@Hamsas-MacBook-Air.local>
2026-05-14 15:46:21 +0800

    Updated account stats models

--- app/models.py
+++ app/models.py
@@ -15,9 +15,6 @@ class User(UserMixin, db.Model):
 
     password_hash = db.Column(db.String(255), nullable=False)
 
-    lowest_us = db.Column(db.Integer, default=9999)
-    average_us = db.Column(db.Float, default=0)
-
     friends = db.relationship(
         'User',
         secondary='friends_table',
