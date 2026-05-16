41bf7a30eb5738a3975a7ecb5918e35ab82f04fa
Hamsa Yusuf <hamsayusuf@Hamsas-MacBook-Air.local>
2026-05-16 21:16:47 +0800

    Refactored account.js with written comments

--- app/static/js/account.js
+++ app/static/js/account.js
@@ -1,8 +1,17 @@
+/* Logout button logic */
+
+/* Handles logout button */
 $(document).ready(() => {
     $("#logout-btn").on("click", function(e) {
         e.preventDefault();
-        sessionStorage.removeItem("minedoku_live_session");
-        console.log("Local puzzle session cache removed.")
+        /* Remove saved live game session */
+        sessionStorage.removeItem(
+            "minedoku_live_session"
+        );
+        console.log(
+            "Local puzzle session cache removed."
+        );
+        /* Redirect user to logout route */
         window.location.href = "/logout";
-    } )
+    });
 });
